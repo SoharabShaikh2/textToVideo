9d37a571f79925514b8fe298cb7354ce68f8de4d
Soharab Shaikh <47937202+SoharabShaikh2@users.noreply.github.com>
2023-05-28 09:23:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import moviepy.video.fx.all as vfx
 import json
+
+from moviepy.audio.AudioClip import CompositeAudioClip
 from moviepy.audio.io.AudioFileClip import AudioFileClip
 
 from moviepy.video.VideoClip import TextClip
@@ -21,33 +23,46 @@ print(jsonData)
 def convertAyatToVideo(jsonData):
     screensize = (1080, 1920)
     clip = VideoFileClip("back.mp4", audio=False)
+    mainAudio = AudioFileClip("back_mp3.mp3")
 
     clip_resized = clip.fx(vfx.resize, width=1080, height=1920)
 
     audio = AudioFileClip(jsonData["ayat_mp3"])
-    videoDuration = audio.duration + (len(jsonData["wbw"]) * 5)
+    audio1 = audio.set_start(0)
+    videoDuration = audio.duration + (len(jsonData["wbw"]) * 5) + 1
 
     clip1 = vfx.loop(clip_resized, duration=videoDuration)
+    mainAudio1 = vfx.loop(mainAudio, duration=videoDuration)
 
     clipArray = []
+    audioArry = []
     clipArray.append(clip1)
+    audioArry.append(mainAudio1)
+    audioArry.append(audio1)
 
     txt_clip = TextClip(jsonData["arabic"], fontsize=100, color='white', align='center', size=screensize,
                         method='caption')
-    txt_clip = txt_clip.set_position('center').set_duration(audio.duration).set_start(0)
+    txt_clip = txt_clip.set_position('center').set_duration(audio.duration+1).set_start(0)
     txt_clip_new = txt_clip.fx(vfx.margin, bottom=600, opacity=0)
-    new_txt_Clip = txt_clip_new.set_audio(audio)
-    clipArray.append(new_txt_Clip)
+
+    txt_clip_new = vfx.fadein(txt_clip_new, 1)
+    #txt_clip_new = vfx.fadeout(txt_clip_new, 1)
+    #new_txt_Clip = txt_clip_new.set_audio(audio)
+    clipArray.append(txt_clip_new)
 
     txt_clip2 = TextClip(jsonData["bengali"], font='Bangla.ttc', fontsize=70, color='white', align='center',
                          size=screensize, method='caption')
-    txt_clip2 = txt_clip2.set_position('center').set_duration(audio.duration).set_start(0)
+    txt_clip2 = txt_clip2.set_position('center').set_duration(audio.duration+1).set_start(0)
+    txt_clip2 = vfx.fadein(txt_clip2, 1)
+    #txt_clip2 = vfx.fadeout(txt_clip2, 1)
     clipArray.append(txt_clip2)
 
     txt_clip3 = TextClip(jsonData["english"], fontsize=70, color='white', align='center', size=screensize,
                          method='caption')
-    txt_clip3 = txt_clip3.set_position('center').set_duration(audio.duration).set_start(0)
+    txt_clip3 = txt_clip3.set_position('center').set_duration(audio.duration+1).set_start(0)
     txt_clip3_new = txt_clip3.fx(vfx.margin, top=600, opacity=0)
+    txt_clip3_new = vfx.fadein(txt_clip3_new, 1)
+    #txt_clip3_new = vfx.fadeout(txt_clip3_new, 1)
     clipArray.append(txt_clip3_new)
 
     wordBword = jsonData["wbw"]
@@ -63,28 +78,36 @@ def convertAyatToVideo(jsonData):
         child_clip1 = TextClip(wordBword[i]["arabic"], fontsize=200, color='white')
         child_clip1 = child_clip1.set_position("center").set_duration(5).set_start(time)
         child_clip1_new = child_clip1.fx(vfx.margin, bottom=600, opacity=0)
+        child_clip1_new = vfx.fadein(child_clip1_new, 1)
+        #child_clip1_new = vfx.fadeout(child_clip1_new, 1)
         child_audio = AudioFileClip(wordBword[i]["ar_mp3"])
-        new_child_clip1 = child_clip1_new.set_audio(child_audio.set_start(time))
+        #new_child_clip1 = child_clip1_new.set_audio(child_audio.set_start(time))
+        child_audio1 = child_audio.set_start(time)
+        audioArry.append(child_audio1)
 
-        clipArray.append(new_child_clip1)
+        clipArray.append(child_clip1_new)
 
         child_clip2 = TextClip(wordBword[i]["bengali"], font='Bangla.ttc', fontsize=100, color='white')
         child_clip2 = child_clip2.set_position("center").set_duration(5).set_start(time)
-
+        child_clip2 = vfx.fadein(child_clip2, 1)
+        #child_clip2 = vfx.fadeout(child_clip2, 1)
         # child_clip2 = child_clip2.margin(20)
         clipArray.append(child_clip2)
 
         child_clip3 = TextClip(wordBword[i]["english"], fontsize=100, color='white')
         child_clip3 = child_clip3.set_position("center").set_duration(5).set_start(time)
         child_clip3_new = child_clip3.fx(vfx.margin, top=600, opacity=0)
+        child_clip3_new = vfx.fadein(child_clip3_new, 1)
+        #child_clip3_new = vfx.fadeout(child_clip3_new, 1)
         # child_clip3 = child_clip3.margin(20)
         clipArray.append(child_clip3_new)
 
         i += 1
 
+    audioMixed = CompositeAudioClip(audioArry)
     # Overlay the text clip on the first video clip
     video = CompositeVideoClip(clipArray)
-
+    video = video.set_audio(audioMixed)
     # showing video
     video.write_videofile("newfile.mp4")
 
